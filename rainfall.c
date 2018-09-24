a8778be08955f2f74a7abb79038d38c9c4886f41
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-24 17:20:13 -0500

    Create rainfall.c

---
+++ rainfall.c
@@ -0,0 +1,12 @@
+/**
+ *Author: SungHa Park
+ *Date: 09/24/2018
+ *This program compute and print correct average rainfall
+ */
+ 
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char **argv) {
+  
