1457cf4d2cb112ebca5189d8d50949b017666fa6
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-28 11:16:59 -0500

    Update rainfall.c

--- rainfall.c
+++ rainfall.c
@@ -9,4 +9,22 @@
 #include<math.h>
 
 int main(int argc, char **argv) {
+ 
+ double averageRainFall, rainfall; 
+ 
+ totalRain = rainfall + totalRain
   
+ printf("Enter the rainfall of this day:\n");
+ scanf("%lf", rainfall);
+       
+ if(rainfall >= 99999) {
+  end 
+ }
+ if(user inputs negative value) {
+  ignore...
+   
+ 
+ while(rainfall != 99999) {
+  totalRain = rainfall + totalRain
+   
+   
