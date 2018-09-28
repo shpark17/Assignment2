3d3b6a902d51ef129c69b19eabaf0090330b8f38
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-28 14:25:23 -0500

    Update rainfall.c

--- rainfall.c
+++ rainfall.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv) {
   
    printf("Enter the rainfall of this day:\n");
    scanf("%lf", rainfall);
+  printf("%f", rainfall);
   
   if(rainfall == 99999) {
    break;
