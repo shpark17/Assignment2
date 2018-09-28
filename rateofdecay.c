2a0a0082fd42edf94df2df96bc3c7c3477607541
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-28 11:25:30 -0500

    Create rateofdecay.c

---
+++ rateofdecay.c
@@ -0,0 +1,11 @@
+/**
+ *Author: SungHa Park
+ *Date: 09/24/2018
+ *This program finds the rate of decay of a radioactive isotope
+ */
+ 
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char **argv) {
