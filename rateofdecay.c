6b6ad681f7f66b8cb3e8d9f2b478a1db0b8649c0
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-09-28 15:41:31 -0500

    Update rateofdecay.c

--- rateofdecay.c
+++ rateofdecay.c
@@ -9,3 +9,19 @@
 #include<math.h>
 
 int main(int argc, char **argv) {
+ 
+ double remains, years;
+ 
+ atomicNumber = atoi(argv[1]);
+ elementName = 
+ elementSymbol = 
+ halflife = atof(argv[4]);
+ initialMass = atof(argv[5]);
+ printf("%s", argv[2]);
+ printf("%s", argv[3])
+ 
+ int i = 0;
+ for(i=0;i<=halflife;i++) {
+  remains = initialmass * pow(.5, (i/halflife));
+ }
+ printf("
